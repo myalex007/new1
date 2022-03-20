f01ec117aeb581295d87d9ae83241b9c65eeb22b
myalex007 <101989123+myalex007@users.noreply.github.com>
2022-03-20 20:21:37 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,137 @@
+<body class="post-template-default single single-post postid-1873 single-format-standard td-standard-pack kak-pisat-test-kejsy-polnoe-rukovodstvo global-block-template-1 td-journal tdb_template_23 white-menu tdb-template tdc-header-template tdc-footer-template td-animation-stack-type0 td-full-layout td-js-loaded" itemscope="itemscope" itemtype="https://schema.org/WebPage">
+
+        <div class="td-scroll-up td-scroll-up-visible"><i class="td-icon-menu-up"></i></div>
+
+    <div class="td-menu-background"></div>
+<div id="td-mobile-nav">
+    <div class="td-mobile-container">
+        <!-- mobile menu top section -->
+        <div class="td-menu-socials-wrap">
+            <!-- socials -->
+            <div class="td-menu-socials">
+                
+        <span class="td-social-icon-wrap">
+            <a target="_blank" href="https://www.facebook.com/testengineer.ru" title="Facebook">
+                <i class="td-icon-font td-icon-facebook"></i>
+            </a>
+        </span>
+        <span class="td-social-icon-wrap">
+            <a target="_blank" href="https://t.me/godoftesting" title="Telegram">
+                <i class="td-icon-font td-icon-telegram"></i>
+            </a>
+        </span>
+        <span class="td-social-icon-wrap">
+            <a target="_blank" href="https://twitter.com/testengineer_ru" title="Twitter">
+                <i class="td-icon-font td-icon-twitter"></i>
+            </a>
+        </span>
+        <span class="td-social-icon-wrap">
+            <a target="_blank" href="https://vk.com/testengineer" title="VKontakte">
+                <i class="td-icon-font td-icon-vk"></i>
+            </a>
+        </span>            </div>
+            <!-- close button -->
+            <div class="td-mobile-close">
+                <a href="#"><i class="td-icon-close-mobile"></i></a>
+            </div>
+        </div>
+
+        <!-- login section -->
+                    <div class="td-menu-login-section">
+                
+    <div class="td-guest-wrap">
+        <div class="td-menu-login"><a id="login-link-mob">вход</a> <span></span><a id="register-link-mob">регистрация</a></div>
+    </div>
+            </div>
+        
+        <!-- menu section -->
+        <div class="td-mobile-content">
+            <div class="menu-td-demo-header-menu-container"><ul id="menu-td-demo-header-menu" class="td-mobile-main-menu"><li id="menu-item-30" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-first menu-item-30"><a href="https://testengineer.ru/">Блог</a></li>
+<li id="menu-item-120" class="menu-item menu-item-type-taxonomy menu-item-object-category current-post-ancestor current-menu-parent current-post-parent menu-item-has-children menu-item-120 td_mobile_submenu td_mobile_elem_with_submenu_0"><a href="https://testengineer.ru/category/obuchenie-testirovanie/" class="td-link-element-after">Обучение<i class="td-icon-menu-right td-element-after"></i></a>
+<ul class="sub-menu">
+	<li id="menu-item-227" class="menu-item menu-item-type-taxonomy menu-item-object-category current-post-ancestor current-menu-parent current-post-parent menu-item-227"><a href="https://testengineer.ru/category/obuchenie-testirovanie/manual-testing/">Ручное тестирование</a></li>
+	<li id="menu-item-226" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-226"><a href="https://testengineer.ru/category/obuchenie-testirovanie/automation-testing/">Автоматизированное тестирование</a></li>
+	<li id="menu-item-2481" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-2481"><a href="https://testengineer.ru/category/obuchenie-testirovanie/project-management/">Проджект-менеджмент</a></li>
+	<li id="menu-item-583" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-583"><a href="https://testengineer.ru/category/obuchenie-testirovanie/unit-testing/">Unit-тестирование</a></li>
+	<li id="menu-item-1548" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1548"><a href="https://testengineer.ru/category/obuchenie-testirovanie/performance-testing/">Тестирование производительности</a></li>
+	<li id="menu-item-1290" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1290"><a href="https://testengineer.ru/category/obuchenie-testirovanie/methodologies/">Процессы и методологии</a></li>
+	<li id="menu-item-1083" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1083"><a href="https://testengineer.ru/category/obuchenie-testirovanie/soft-skills/">Soft skills</a></li>
+</ul>
+</li>
+<li id="menu-item-121" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-121"><a href="https://testengineer.ru/category/sobesedovanie/">Собеседование</a></li>
+<li id="menu-item-122" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-122"><a href="https://testengineer.ru/category/testy-testirovanie/">Тесты</a></li>
+<li id="menu-item-2298" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2298"><a href="https://testengineer.ru/jobs/">💼 Вакансии</a></li>
+</ul></div>        </div>
+    </div>
+
+    <!-- register/login section -->
+            <div id="login-form-mobile" class="td-register-section">
+            
+            <div id="td-login-mob" class="td-login-animation td-login-hide-mob">
+            	<!-- close button -->
+	            <div class="td-login-close">
+	                <a href="#" class="td-back-button"><i class="td-icon-read-down"></i></a>
+	                <div class="td-login-title">вход</div>
+	                <!-- close button -->
+		            <div class="td-mobile-close">
+		                <a href="#"><i class="td-icon-close-mobile"></i></a>
+		            </div>
+	            </div>
+	            <div class="td-login-form-wrap">
+	                <div class="td-login-panel-title"><span>Добро пожаловат!</span>Войдите в свой аккаунт</div>
+	                <div class="td_display_err"></div>
+	                <div class="td-login-inputs"><input class="td-login-input" type="text" name="login_email" id="login_email-mob" value="" required=""><label>Ваше имя пользователя</label></div>
+	                <div class="td-login-inputs"><input class="td-login-input" type="password" name="login_pass" id="login_pass-mob" value="" required=""><label>Ваш пароль</label></div>
+	                <input type="button" name="login_button" id="login_button-mob" class="td-login-button" value="ВОЙТИ">
+	                <div class="td-login-info-text">
+	                <a href="#" id="forgot-pass-link-mob">Вы забыли свой пароль?</a>
+	                </div>
+	                <div class="td-login-register-link">
+	                <a id="signin-register-link-mob">завести аккаунт</a>
+	                </div>
+	                
+	                
+
+                </div>
+            </div>
+
+            
+            
+            
+        
+
+
+</style>
+<style>
+/* custom css */
+.tdi_72_23c .tdm-btn{
+                    height: auto;
+                    padding: 10px 26px;
+                }
+
+/* phone */
+@media (max-width: 767px){
+.tdi_72_23c .tdm-btn{
+                    height: auto;
+                    padding: 0px 26px;
+                }
+}
+</style>
+<style>
+body .tdi_73_666{
+					background-color: #0088cc;
+				}.tdi_73_666 i{
+					font-size: 18px;
+				}.tdi_73_666 i:first-child{
+					margin-right: 8px;
+				}.tdi_73_666{
+					font-family:Montserrat !important;font-size:16px !important;font-weight:600 !important;text-transform:uppercase !important;
+				}
+
+
+
+
+<!-- Dynamic page generated in 0.652 seconds. -->
+<!-- Cached page generated by WP-Super-Cache on 2022-03-20 16:22:10 -->
+
+<!-- Compression = gzip --></body>
